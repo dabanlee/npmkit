80ea3b43a524479fff69f488368c3bf47ec8a03f
大板栗 <576839360@qq.com>
2019-08-01 16:41:58 +0800

    📦 Add package `rollup-plugin-commonjs`

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,17 @@
-import alias from 'rollup-plugin-alias';
-import minify from 'rollup-plugin-babel-minify';
-import resolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript';
+import alias from 'rollup-plugin-alias'
+import minify from 'rollup-plugin-babel-minify'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript'
+import commonjs from 'rollup-plugin-commonjs'
 
-const isProd = process.env.NODE_ENV === 'production';
-const { moduleName, name: fileName } = require('./package.json');
-const getFilePath = (type = '') => `dist/${fileName}${type == '' ? '' : '.'}${type}.js`;
+const isProd = process.env.NODE_ENV === 'production'
+const { moduleName, name: fileName } = require('./package.json')
+const getFilePath = (type = '') => `dist/${fileName}${type == '' ? '' : '.'}${type}.js`
 const output = options => ({
     name: moduleName,
     sourcemap: true,
     ...options,
-});
+})
 
 const configure = {
     input: 'src/index.ts',
@@ -26,18 +27,19 @@ const configure = {
             common: './common',
         }),
         typescript(),
+        commonjs(),
         resolve(),
     ],
     external: [],
-};
+}
 
 if (isProd) {
     configure.output = configure.output.map(output => {
-        const format = output.format == 'umd' ? '' : `.${output.format}`;
-        output.file = `dist/${fileName}${format}.min.js`;
-        return output;
-    });
-    configure.plugins.push(minify());
+        const format = output.format == 'umd' ? '' : `.${output.format}`
+        output.file = `dist/${fileName}${format}.min.js`
+        return output
+    })
+    configure.plugins.push(minify())
 }
 
-module.exports = configure;
+module.exports = configure
